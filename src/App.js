bb9012029a6ea57fac26a01bafd450950adf47ca
Elisha Flacon <elishaflacon@gmail.com>
2023-03-16 20:49:32 +0500

    add new functional

--- src/App.js
+++ src/App.js
@@ -9,88 +9,115 @@ function App() {
 
 
     const buttonClick = (event) => {
-        if (event.target.textContent === 'C()/789x456-123+0%.=') {
+        // эта фигня нужна чтобы при нажатии на промежуток 
+        // между циферками, у нас не вводилось это значение
+        if (event.target.textContent === 'C()/789x456-123+0.=') {
             return;
         }
+
         if (event.target.textContent === 'C') {
             clearInput();
             return;
         }
+
         if (event.target.textContent === '=') {
-            calculate(input);
+            const refreshInput = refresh(input);
+            calculate(refreshInput);
             return;
         }
+
         if (input === '0') {
             setInput(event.target.textContent);
             return;
         }
-        if (input[input.length - 1] !== ['+', '-', 'x', '/']) {
-            setInput(input + event.target.textContent);
-            return;
-        }
-        setInput(input + " " + event.target.textContent);
+
+        setInput(input + event.target.textContent);
     }
 
 
+    // чистим ввод
     const clearInput = () => {
         setInput('0');
-        // setInputInfo('0');
+        setInputInfo('0');
     }
 
 
-    // тут повторяется кусок кода с нумсами, сделай отдельную функцию!
+    // после того как посчитали 2 числа, мы должны их стереть
+    // и вставить на это место полученное значение
+    const updateArray = (nums, idx, count) => {
+        nums.splice(idx, 1);
+        nums.splice(idx - 1, 1);
+        nums.splice(idx - 1, 1);
+        nums.splice(idx - 1, 0, count);
+    }
 
+
+    // считаем
     const calculate = (nums) => {
-        let count = 0;
-        nums = refresh(nums).split(" ");
-        console.log(nums);
+        let count = 0
+
+        while (nums.indexOf('(') !== -1) {
+            const openBracket = nums.lastIndexOf('(');
+            const closeBracket = nums.indexOf(')');
+
+            // проверки на то что есть не закрытая скобка
+            if (openBracket > closeBracket) {
+                setInputInfo('Ошибка!');
+                return;
+            }
+            if (closeBracket === -1) {
+                setInputInfo('Ошибка!');
+                return;
+            }
+
+            count = parenthesis(nums);
+
+            nums.splice(openBracket, closeBracket - openBracket + 1, count);
+        }
+
         while (nums.indexOf('x') !== -1) {
             const idx = nums.indexOf('x');
             count = multiplier(nums);
-            nums.splice(idx, 1);
-            nums.splice(idx - 1, 1);
-            nums.splice(idx - 1, 1);
-            nums.splice(idx - 1, 0, count);
+
+            updateArray(nums, idx, count);
         }
         while (nums.indexOf('/') !== -1) {
             const idx = nums.indexOf('/');
             count = division(nums);
-            nums.splice(idx, 1);
-            nums.splice(idx - 1, 1);
-            nums.splice(idx - 1, 1);
-            nums.splice(idx - 1, 0, count);
+
+            updateArray(nums, idx, count);
         }
         while (nums.indexOf('+') !== -1 || nums.indexOf('-') !== -1) {
             if (nums.indexOf('+') < nums.indexOf('-') && nums.indexOf('+') !== -1 && nums.indexOf('-') !== -1) {
                 const idx = nums.indexOf('+');
                 count = plus(nums);
-                nums.splice(idx, 1);
-                nums.splice(idx - 1, 1);
-                nums.splice(idx - 1, 1);
-                nums.splice(idx - 1, 0, count);
-            } else if (nums.indexOf('+') !== -1 && nums.indexOf('-') === -1) {
+
+                updateArray(nums, idx, count);
+            }
+            else if (nums.indexOf('+') !== -1 && nums.indexOf('-') === -1) {
                 const idx = nums.indexOf('+');
                 count = plus(nums);
-                nums.splice(idx, 1);
-                nums.splice(idx - 1, 1);
-                nums.splice(idx - 1, 1);
-                nums.splice(idx - 1, 0, count);
-            } else if (nums.indexOf('-') !== -1) {
+
+                updateArray(nums, idx, count);
+            }
+            else if (nums.indexOf('-') !== -1) {
                 const idx = nums.indexOf('-');
                 count = minus(nums);
-                nums.splice(idx, 1);
-                nums.splice(idx - 1, 1);
-                nums.splice(idx - 1, 1);
-                nums.splice(idx - 1, 0, count);
+
+                updateArray(nums, idx, count);
             }
         }
-        console.log(count);
+
         setInputInfo(String(count));
-        clearInput();
+        setInput(String(count));
+        // clearInput();
+
         return count;
     }
 
+
     // тут мы добовляем пробелы в строку
+    //  и превращаем в массив
     const refresh = (nums) => {
         let output = '';
         nums = nums.split('');
@@ -112,20 +139,29 @@ function App() {
                     output += ' + ';
                     nums.splice(0, 1);
                     break;
+                case '(':
+                    output += '( ';
+                    nums.splice(0, 1);
+                    break;
+                case ')':
+                    output += ' )';
+                    nums.splice(0, 1);
+                    break;
                 default:
                     output += nums[0];
                     nums.splice(0, 1);
             }
         }
-        return output;
+
+        return output.split(" ");
     }
+
 
     const multiplier = (nums) => {
         const idx = nums.indexOf('x');
         const count = Number(nums[idx - 1]) * Number(nums[idx + 1]);
         return (count);
     }
-
 
     const division = (nums) => {
         const idx = nums.indexOf('/');
@@ -146,48 +182,59 @@ function App() {
     }
 
 
+    // считаем выражение внутри скобки
+    const parenthesis = (nums) => {
+        const openBracket = nums.lastIndexOf('(');
+        const closeBracket = nums.indexOf(')');
+
+        const bracketNums = nums.slice(openBracket + 1, closeBracket);
+
+        const count = calculate(bracketNums);
+
+        return (count);
+    }
+
+
     return (
         <div className="app">
             <h1>
-                МЕГАКАЛЬКУЛЯТОР v1.5
+                МЕГАКАЛЬКУЛЯТОР 2.0
             </h1>
-            <div className="app__input-form">
-                <form className="form" action="number">
-                    <div className="form__input-space">
-                        <div className="form__info">
-                            {inputInfo}
-                        </div>
-                        <div className="form__input" onClick={() => refresh("10-4+9x8/9-10-999/2x5")}>
-                            {input}
-                        </div>
+
+            <form className="form" action="number">
+                <div className="form__input-space">
+                    <div className="form__info">
+                        {inputInfo}
                     </div>
-                </form>
-                <div className="app__keyboard">
-                    <div className="keyboard" onClick={(e) => buttonClick(e)}>
-                        <button className="keyborad__button clear">C</button>
-                        <button className="keyborad__button dont-working">(</button>
-                        <button className="keyborad__button dont-working">)</button>
-                        <button className="keyborad__button yellow">/</button>
-                        <button className="keyborad__button">7</button>
-                        <button className="keyborad__button">8</button>
-                        <button className="keyborad__button">9</button>
-                        <button className="keyborad__button yellow">x</button>
-                        <button className="keyborad__button">4</button>
-                        <button className="keyborad__button">5</button>
-                        <button className="keyborad__button">6</button>
-                        <button className="keyborad__button yellow">-</button>
-                        <button className="keyborad__button">1</button>
-                        <button className="keyborad__button">2</button>
-                        <button className="keyborad__button">3</button>
-                        <button className="keyborad__button yellow">+</button>
-                        <button className="keyborad__button">0</button>
-                        <button className="keyborad__button dont-working">%</button>
-                        <button className="keyborad__button">.</button>
-                        <button className="keyborad__button yellow">=</button>
+                    <div className="form__input">
+                        {input}
                     </div>
                 </div>
+            </form>
+
+            <div className="keyboard" onClick={(e) => buttonClick(e)}>
+                <button className="keyborad__button clear">C</button>
+                <button className="keyborad__button">(</button>
+                <button className="keyborad__button">)</button>
+                <button className="keyborad__button yellow">/</button>
+                <button className="keyborad__button">7</button>
+                <button className="keyborad__button">8</button>
+                <button className="keyborad__button">9</button>
+                <button className="keyborad__button yellow">x</button>
+                <button className="keyborad__button">4</button>
+                <button className="keyborad__button">5</button>
+                <button className="keyborad__button">6</button>
+                <button className="keyborad__button yellow">-</button>
+                <button className="keyborad__button">1</button>
+                <button className="keyborad__button">2</button>
+                <button className="keyborad__button">3</button>
+                <button className="keyborad__button yellow">+</button>
+                <button className="keyborad__button big-button">0</button>
+                <button className="keyborad__button">.</button>
+                <button className="keyborad__button yellow">=</button>
             </div>
         </div>
+
     );
 }
 
